01a9c712edcdd1ebe41cad8c4bd6e0603536dd36
xuejike <245674286@qq.com>
2019-01-21 18:43:19 +0800

    readme 文档中增加使用方式

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
-# Word-Tpl Word模板引擎
+# Word-Tpl Word模板引擎[![](https://jitpack.io/v/xuejike/word-tpl.svg)](https://jitpack.io/#xuejike/word-tpl)
+
 如果只是在word中替换变量，那是我们没用充分利用起word的功能，word-tpl实现了在word中使用我们常用的模板引擎来对word进行内容控制，比如if判断，for循环等，让生成报告更加方便简单。word-tpl中word操作采用POI进行编辑，而不是采用xml生成方式生成的，这样生成的word更加标准。
 # 功能说明
 * [x] 变量替换
@@ -14,7 +15,36 @@
 ## 转换后的效果
 ![avatar](./word-tpl/Snipaste_2019-01-21_12-56-19.png)
 # 使用方式
+## 引入类库
+```groovy
 
+
+repositories {
+    maven { url 'https://jitpack.io' }
+
+}
+
+dependencies {
+    implementation 'com.github.xuejike.word-tpl:word-tpl-core:bea94c8e07'
+}
+
+
+```
+## 生成word文档
+```java
+ public class TestMain {
+     public static void main(String[] args) throws IOException, TplBuildException {
+         String tplFile = "E:\\project\\word-tpl\\word-tpl\\var-tpl.docx";
+         String outFile = "E:\\project\\word-tpl\\word-tpl\\var-tpl-out.docx";
+ 
+         HashMap<String, Object> map = new HashMap<>();
+         map.put("xuejike","薛纪克");
+         WordTplUtils wordTplUtils = new WordTplUtils(new FreemarkerWordTplFactory());
+         wordTplUtils.buildWord(new File(tplFile),outFile,map);
+     }
+ }
+
+```
 # 设计思想
 
 # 二次开发
--- word-tpl-demo/src/main/java/TestMain.java
+++ word-tpl-demo/src/main/java/TestMain.java
@@ -17,33 +17,13 @@ import java.util.*;
  * @author xuejike
  */
 public class TestMain {
-
-    public static int status = 0;
-
     public static void main(String[] args) throws IOException, TplBuildException {
         String tplFile = "E:\\project\\word-tpl\\word-tpl\\var-tpl.docx";
         String outFile = "E:\\project\\word-tpl\\word-tpl\\var-tpl-out.docx";
-//        FileInputStream fileInputStream = new FileInputStream(tplFile);
-//
-//        WordParse wordParse = new WordParse(new FreemarkerTokenParse());
-//        StringBuilder tpl = wordParse.word2TplScript(fileInputStream);
-//        System.out.println(tpl);
-//        FreemarkerWordTplFactory tplFactory = new FreemarkerWordTplFactory();
-//        tplFactory.addTpl("test",tpl.toString());
+
         HashMap<String, Object> map = new HashMap<>();
         map.put("xuejike","薛纪克");
-////        String execTplName = tplFactory.execTplName("test", map);
-//        WordBuild wordBuild = new WordBuild(tplFactory);
-//        XWPFDocument xwpfDocument = wordBuild.execScript(tplFile, tpl.toString(), map);
-//        xwpfDocument.write(new FileOutputStream(outFile));
-//        System.out.println(execTplName);
-
-//        officeToPDF("E:\\project\\word-tpl-script\\儿童报告_网络版_12-25.docx","E:\\project\\word-tpl-script\\word-table_out.pdf");
         WordTplUtils wordTplUtils = new WordTplUtils(new FreemarkerWordTplFactory());
-//        String tplScript = wordTplUtils.buildTplScript(tplFile);
-//        System.out.println(tplScript);
         wordTplUtils.buildWord(new File(tplFile),outFile,map);
     }
-
-
 }
